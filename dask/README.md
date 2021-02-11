52d11da171b703c98dfca064f79483360642c713
Dileep Holla <dileep.holla@gmail.com>
2021-02-11 13:37:43 -0500

    pushing dash app

---
+++ dask/README.md
@@ -0,0 +1,49 @@
+## Dask
+
+Dask is designed to extend the numpy and pandas packages to work on data processing problems that are too large to be kept in memory. It breaks the larger processing job into many smaller tasks that are handled by numpy or pandas and then it reassembles the results into a coherent whole.
+Dask is a flexible library for parallel computing in Python. (1)
+
+Dask is composed of two parts:
+
+- Dynamic task scheduling optimized for computation. This is similar to Airflow, Luigi, Celery, or Make, but optimized for interactive computational workloads.
+- “Big Data” collections like parallel arrays, dataframes, and lists that extend common interfaces like NumPy, Pandas, or Python iterators to larger-than-memory or distributed environments. These parallel collections run on top of dynamic task schedulers.
+
+As datasets and computations scale faster than CPUs and RAM, we need to find ways to scale our computations across multiple machines. This introduces many new concerns:
+
+- How to have computers talk to each other over the network?
+- How and when to move data between machines?
+- How to recover from machine failures?
+- How to deploy on an in-house cluster?
+- How to deploy on the cloud?
+- How to deploy on an HPC super-computer?
+- How to provide an API to this system that users find intuitive?
+
+While it is possible to build these systems in-house (and indeed, many exist), many organizations increasingly depend on solutions developed within the open source community. These tend to be more robust, secure, and fully featured without being tended by in-house staff.
+
+Dask solves the problems above. It figures out how to break up large computations and route parts of them efficiently onto distributed hardware. Dask is routinely run on thousand-machine clusters to process hundreds of terabytes of data efficiently within secure environments. (2)
+
+References: <br>
+(1) https://docs.dask.org/en/latest/ <br>
+(2) https://docs.dask.org/en/latest/why.html
+
+### Requirements 
+
+```
+pip install "dask[complete]"
+pip install graphviz
+pip install pandas
+pip install tqdm
+```
+
+You must install both the graphviz system library (with tools like `apt-get`, `yum`, or `brew`) and the graphviz Python library. Download the executables from here: https://www.graphviz.org/download/
+
+### Getting Data
+This lab uses the [NYC Taxi Dataset](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page) - since this data is already is available on S3 and we have our AWS CLI configured, get the data using the following CLI command:
+```
+aws s3 cp "s3://nyc-tlc/trip data/yellow_tripdata_2018-04.csv" "tripdata.csv"
+```
+You may use the data from any Month/Year - but we will be sticking with data from April 2018 for this tutorial.
+
+### Tutorial
+
+Complete step-by-step instructions and explanations are documented in the `dask_lab.ipynb` Jupyter notebook.
---
+++ plotly-dash/dash-basic/dash_app.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import plotly.graph_objs as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+
+# Define the regions and the full name
+regions = {'CA': 'Canada', 'GB': 'Great Britain', 'HK': 'Hong Kong',
+           'JP': 'Japan', 'KR': 'South Korea', 'US': 'United States',
+           'WorldWide': 'World Wide'}
+region2brief = dict([(regions[k], k) for k in regions])
+
+# Read multiple data set
+filepath = 'raw_data/Console_share_'
+df = []
+for region in regions:
+	filepath_curr = filepath + region + '.csv'
+	df_temp = pd.read_csv(filepath_curr)
+	rows = df_temp.shape[0]
+	df_temp['Region'] = [regions[region] for _ in range(rows)]
+	df.append(df_temp)
+
+df = pd.concat(df)
+df = df.drop('Other', axis=1)
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m')
+
+
+# Dash Set up
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# Dropdown list and radio button options set up
+region_dropdown = [{'label': regions[region], 'value': regions[region]} \
+                   for region in regions]
+vis_type_options = [{'label': 'Line Chart', 'value': 'line'},
+                {'label': 'Bar Chart', 'value': 'bar'}]
+
+# Declare headline and description
+headline = 'Gaming Console Marketshare in 2018'
+description = 'Sample Dash App w/ bar & line charts'
+
+# Dashboard layout
+app.layout = html.Div([
+	html.H1(children=headline, style={'text-align':'center'}), 
+	# Position 0, headline
+	html.Div(children=description, style={'width':'60%'}), 
+	# Position 1, descritpion
+	html.Div([
+		html.Div([
+			html.P('Select Region:'),
+			dcc.Dropdown(
+				id='region-dropdown',
+				options=region_dropdown,
+				value='World Wide',
+				style={'width': '90%'}
+				)
+			], style={'width':'60%', 'display': 'inline-block',
+			          'vertical-align': 'middle'}),
+		html.Div([
+			html.P('Select Visualization:'),
+			dcc.RadioItems(
+				id='type-radio',
+				options=vis_type_options,
+				value='line'
+				)
+			], style={'width':'40%', 'display': 'inline-block',
+			          'vertical-align': 'middle'})
+	]), # Position 2, Options
+	html.Div(dcc.Graph(id='vis')), # Position 3, Graph
+	html.Div(dcc.Markdown(id='markdown'), style={'width':'60%'}) 
+	# Position 4, Markdown
+]) # End Dashboard Div
+
+def getLineChart(df, title):
+	traces = []
+	for console in df.columns:
+		if console != 'Date' and console != 'Region':
+			traces.append(dict(
+				x=df['Date'],
+				y=df[console],
+				mode='lines+markers',
+				marker={
+						'size': 15,
+						'line': {'width': 0.5, 'color': 'white'}
+				},
+				name=console
+				))
+	layout = dict(title=title, 
+		          xaxis={'title':'Date'},
+		          yaxis={'title':'Market Share (%)', 'range': [0,100]},
+		          transition={'duration': 500})
+	return {'data': traces, 'layout': layout}
+
+def getBarChart(df, title):
+	df = df.drop(['Region'], axis=1)
+	data = []
+	for console in df.columns:
+		if console != 'Date':
+			temp = {}
+			temp['x'] = df['Date']
+			temp['y'] = df[console]
+			temp['type'] = 'bar'
+			temp['name'] = console
+			data.append(temp)
+
+	layout = dict(title=title, 
+		          xaxis={'title':'Consoles'},
+		          yaxis={'title':'Market Share (%)', 'range': [0,100]},
+		          barmode='group',
+		          transition={'duration': 500})
+
+	return {'data': data, 'layout': layout}
+
+@app.callback([Output('vis','figure'), Output('markdown','children')],
+	          [Input('region-dropdown','value'),
+	           Input('type-radio','value')])
+def display_graph(region, vis_type):
+	df_temp = df[df['Region']==region]
+	fig = None
+	vis_title = 'Gaming Market Share in 2018'
+	if vis_type == 'line':
+		fig = getLineChart(df_temp, vis_title)
+	elif vis_type == 'bar':
+		fig = getBarChart(df_temp, vis_title)
+	filepath_markdown = 'raw_data/Markdown_'
+	filepath_markdown += region2brief[region]
+	filepath_markdown += '.txt'
+	f = open(filepath_markdown, 'r')
+	text = 'A brief analysis: '
+	text += f.read()
+	f.close()
+	return fig, text
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=1200)
