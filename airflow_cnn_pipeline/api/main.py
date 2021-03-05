6c2743aa409dd29401d7893f9b75e85f1a9d4a53
Dileep Holla <dileep.holla@gmail.com>
2021-03-05 16:32:39 -0500

    fixed relative imports

--- airflow_cnn_pipeline/api/main.py
+++ airflow_cnn_pipeline/api/main.py
@@ -1,10 +1,18 @@
+import inspect
 import os
+import sys
+
 from fastapi import FastAPI
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from get_image import download
 from predict import get_score
 
-
 app = FastAPI()
+
 
 @app.get("/predict/{img_url:path}")
 def get_inference(img_url: str):
@@ -18,11 +26,13 @@ def get_inference(img_url: str):
     # Return the issue & confidence
     return a
 
+
 @app.post("/pipeline/start")
 def start_pipeline():
     run_details = os.system('airflow dags trigger CNN-Training-Pipeline')
     # Return the issue & confidence
     return {"status": "Airflow Pipeline Started", "details": run_details}
+
 
 @app.post("/pipeline/config")
 def start_server():
